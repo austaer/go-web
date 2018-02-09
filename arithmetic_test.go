1854831ac5eb4d4199d027d57359859567f3a3fc
austaer <austaer@gmail.com>
2018-02-09 16:24:32 +0800

    [modify] modify test case

--- arithmetic_test.go
+++ arithmetic_test.go
@@ -4,10 +4,6 @@ import (
 	"testing"
 )
 
-func Test_Division_2(t *testing.T) {
-	t.Error("FAILED")
-}
-
 func Test_Division_1(t *testing.T) {
 	if i, e := Division(6, 2); i != 3 || e != nil {
 		t.Error("failed")
